389e07ba840bde89c59355ee3d56216b88e17c77
Kord Maurer <kord.r.maurer@gmail.com>
2023-03-27 10:06:15 -0500

    Created firstName and lastName variables, added them to fullName variable

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -1,11 +1,11 @@
 // REQUIRED FEATURES:
 // 1 - Create a variable called `firstName` and assign it the value of your first name
-
+const firstName = "Kord"
 // 2 - Create a second variable called `lastName` and assign it the value of your last name
-
+const lastName = "Maurer"
 // 3 - Create a third variable called `fullName`, assign it the value of your first and last name
 // (remember, you already have variables for this, can you use those?)
-
+const fullName = firstName + lastName 
 // 4 - Console log the value of `fullName`
 
 // 5 - Create a variable called `luckyNumber` and assign it the value of your lucky number.
